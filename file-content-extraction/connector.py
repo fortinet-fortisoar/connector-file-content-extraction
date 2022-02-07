148e7e1149a1c89d1d08bc3b1ae4e4ce4f77286d
nailix <naili@email.com>
2022-02-07 18:06:48 +0400

    1.0.1a1

--- file-content-extraction/connector.py
+++ file-content-extraction/connector.py
@@ -3,9 +3,8 @@ from .operations import operations, _check_health
 
 logger = get_logger('text_extractor')
 
-
 class TextExtractor(Connector):
-
+    """Main Class"""
     def execute(self, config, operation, params, **kwargs):
         params.update({"operation":operation}) 
         action = operations.get(operation)
